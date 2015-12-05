e28842452db97346d40a6701d8e2e8ce932bac7a
John <haplo31@neuf.fr>
2015-12-05 16:03:59 +0100

    First actions buttons

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,99 +2,71 @@
 
 angular.module('modiPicsApp')
   .controller('MainCtrl', function ($scope, $http, socket,$timeout,$modal) {
-    $scope.LPics = [ //{name:"licorne",src:"http://images.alphacoders.com/136/thumb-350-136423.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"paysage",src:"http://www.picturalium.com/photophotos/telecharger/258"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"logo",src:"http://musique-libre-de-droit.cowblog.fr/images/4868.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"spectacle",src:"http://vincent-presse.com/dossiers/SEA_GIRLS_FIN_DU_MONDE_SITE/hds/SEA%20GIRLS%20FIN%20DU%20MONDE%202010%20OMBRELLE%20libre%20de%20droit.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"mariage",src:"http://2l-image.com/wpimages/wp0753ce26_05_06.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"bebe",src:"http://www.kadolog.com/sites/default/files/img/home-slider-liste-de-naissance-1.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"plat",src:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSx_i-WROPoU07q5_qujRQBweH860k4YZ7ArigWRqK1LwPM2SCg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"bebe",src:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQeiflQvYH2EREjSNXLGBi3qcTralhmNnaPYp9OX6OZo6L7tB-9"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"bateau",src:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRqiw0_7y5GMBiizw7ygj55un0IItExW1Gx0uPmqd8NSRtK2ze9EA"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"sumo",src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7MLr5aTk89Ynl4rntVWSRjEbPwXShAOxPJaHx4f7ayIWtigfy"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"old",src:"http://www.doityvette.fr/wp-content/uploads/2014/07/new-old-stock-3.png"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"portrait",src:"http://patiange.p.a.pic.centerblog.net/33acfa89.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"plage",src:"http://www.photosgratuiteslibresdedroits.com/photosgratuites/376_plage_mont_choisy_mini_640.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"jardin",src:"http://www.picturalium.com/photophotos/telecharger/815"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"famille",src:"http://thumbs.dreamstime.com/z/famille-heureux-775568.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"mariage",src:"http://img.over-blog.com/600x397/4/35/94/82/Carole/Mariage-Carole-Yann---Famille---JDerache--98-.JPG"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"portrait",src:"https://www.pascalboegli.com/wp-content/uploads/malawi/49-portrait-enfant-malawi.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"chat",src:"http://www.enfant.com/pictures/articles/0000/310.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"skate",src:"http://www.college-romainrolland-pontivy.fr/IMG/jpg/skate.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"chien",src:"http://www.chien-guide.com/IMG/jpg/chien_4_.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"dessin",src:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS6qvJm1JynPFbBKlwwGOZ5RO_pIinFXLXPEF6oQCd7C7GawAvwHg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"famille",src:"https://www.higheroneaccount.com/images/student/outpages/photo-free.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"shoot",src:"http://static.independent.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2015/07/28/15/Happy-birthday.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"pont",src:"http://www.freefoto.com/images/9912/01/9912_01_1095---Tyne-Bridge--Sunset_web.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"mariage",src:"http://mattetsuzie.com/wp-content/uploads/2014/08/Photo-Mariage-Auberge-St-Antoine_Vieux-Quebec_15.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"golf",src:"http://visit-hungary.com/site/upload/2009/12/Golf.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"me",src:"https://avatars3.githubusercontent.com/u/4218815?v=3&s=460"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"oldpeople",src:"http://www.gettyimages.com/gi-resources/images/RoyaltyFree/109720793.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"fun",src:"http://media-cdn.tripadvisor.com/media/photo-s/01/1c/7a/d7/cancun.jpg"},
-    //                     {owner:"John",artist:"Protoshop",vote:0,name:"unicorn",src:"http://wallbasehq.net/images/medium/a-rainbow_unicorn-1526281.jpg"}
-    ];
-    $scope.MVPics = [   {owner:"John",artist:"Protoshop",vote:0,name:"spectacle",src:"http://vincent-presse.com/dossiers/SEA_GIRLS_FIN_DU_MONDE_SITE/hds/SEA%20GIRLS%20FIN%20DU%20MONDE%202010%20OMBRELLE%20libre%20de%20droit.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"mariage",src:"http://2l-image.com/wpimages/wp0753ce26_05_06.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"plage",src:"http://www.photosgratuiteslibresdedroits.com/photosgratuites/376_plage_mont_choisy_mini_640.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"jardin",src:"http://www.picturalium.com/photophotos/telecharger/815"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"famille",src:"http://thumbs.dreamstime.com/z/famille-heureux-775568.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"mariage",src:"http://img.over-blog.com/600x397/4/35/94/82/Carole/Mariage-Carole-Yann---Famille---JDerache--98-.JPG"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"portrait",src:"https://www.pascalboegli.com/wp-content/uploads/malawi/49-portrait-enfant-malawi.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"chat",src:"http://www.enfant.com/pictures/articles/0000/310.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"skate",src:"http://www.college-romainrolland-pontivy.fr/IMG/jpg/skate.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"chien",src:"http://www.chien-guide.com/IMG/jpg/chien_4_.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"dessin",src:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS6qvJm1JynPFbBKlwwGOZ5RO_pIinFXLXPEF6oQCd7C7GawAvwHg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"famille",src:"https://www.higheroneaccount.com/images/student/outpages/photo-free.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"shoot",src:"http://static.independent.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2015/07/28/15/Happy-birthday.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"pont",src:"http://www.freefoto.com/images/9912/01/9912_01_1095---Tyne-Bridge--Sunset_web.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"mariage",src:"http://mattetsuzie.com/wp-content/uploads/2014/08/Photo-Mariage-Auberge-St-Antoine_Vieux-Quebec_15.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"golf",src:"http://visit-hungary.com/site/upload/2009/12/Golf.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"me",src:"https://avatars3.githubusercontent.com/u/4218815?v=3&s=460"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"oldpeople",src:"http://www.gettyimages.com/gi-resources/images/RoyaltyFree/109720793.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"fun",src:"http://media-cdn.tripadvisor.com/media/photo-s/01/1c/7a/d7/cancun.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"unicorn",src:"http://wallbasehq.net/images/medium/a-rainbow_unicorn-1526281.jpg"}
-                        
-
-    ];
-    $scope.MCPics = [ {owner:"John",artist:"Protoshop",vote:0,name:"bateau",src:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRqiw0_7y5GMBiizw7ygj55un0IItExW1Gx0uPmqd8NSRtK2ze9EA"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"sumo",src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7MLr5aTk89Ynl4rntVWSRjEbPwXShAOxPJaHx4f7ayIWtigfy"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"old",src:"http://www.doityvette.fr/wp-content/uploads/2014/07/new-old-stock-3.png"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"portrait",src:"http://patiange.p.a.pic.centerblog.net/33acfa89.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"plage",src:"http://www.photosgratuiteslibresdedroits.com/photosgratuites/376_plage_mont_choisy_mini_640.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"jardin",src:"http://www.picturalium.com/photophotos/telecharger/815"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"famille",src:"http://thumbs.dreamstime.com/z/famille-heureux-775568.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"mariage",src:"http://img.over-blog.com/600x397/4/35/94/82/Carole/Mariage-Carole-Yann---Famille---JDerache--98-.JPG"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"portrait",src:"https://www.pascalboegli.com/wp-content/uploads/malawi/49-portrait-enfant-malawi.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"chat",src:"http://www.enfant.com/pictures/articles/0000/310.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"skate",src:"http://www.college-romainrolland-pontivy.fr/IMG/jpg/skate.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"chien",src:"http://www.chien-guide.com/IMG/jpg/chien_4_.jpg"},
-                        {owner:"John",artist:"Protoshop",vote:0,name:"dessin",src:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS6qvJm1JynPFbBKlwwGOZ5RO_pIinFXLXPEF6oQCd7C7GawAvwHg"}
-    ];
-    $scope.showLP=true;
-    $scope.showMVP=false;
-    $scope.showMCP=false;
+    // $scope.Pics = [ 
+    //                     {owner:"John",artist:"Protoshop",modtype:"Adding character",vote:0,name:"paysage",src:"http://www.picturalium.com/photophotos/telecharger/258"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Removing character",vote:0,name:"logo",src:"http://musique-libre-de-droit.cowblog.fr/images/4868.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Adding character",vote:0,name:"spectacle",src:"http://vincent-presse.com/dossiers/SEA_GIRLS_FIN_DU_MONDE_SITE/hds/SEA%20GIRLS%20FIN%20DU%20MONDE%202010%20OMBRELLE%20libre%20de%20droit.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Adding character",vote:0,name:"mariage",src:"http://2l-image.com/wpimages/wp0753ce26_05_06.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Creation",vote:0,name:"bebe",src:"http://www.kadolog.com/sites/default/files/img/home-slider-liste-de-naissance-1.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Improvements",vote:0,name:"plat",src:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSx_i-WROPoU07q5_qujRQBweH860k4YZ7ArigWRqK1LwPM2SCg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Creation",vote:0,name:"bebe",src:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQeiflQvYH2EREjSNXLGBi3qcTralhmNnaPYp9OX6OZo6L7tB-9"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Removing character",vote:0,name:"bateau",src:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRqiw0_7y5GMBiizw7ygj55un0IItExW1Gx0uPmqd8NSRtK2ze9EA"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Creation",vote:0,name:"sumo",src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7MLr5aTk89Ynl4rntVWSRjEbPwXShAOxPJaHx4f7ayIWtigfy"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Creation",vote:0,name:"old",src:"http://www.doityvette.fr/wp-content/uploads/2014/07/new-old-stock-3.png"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Removing character",vote:0,name:"portrait",src:"http://patiange.p.a.pic.centerblog.net/33acfa89.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Creation",vote:0,name:"plage",src:"http://www.photosgratuiteslibresdedroits.com/photosgratuites/376_plage_mont_choisy_mini_640.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Inscrustation",vote:0,name:"jardin",src:"http://www.picturalium.com/photophotos/telecharger/815"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Adding character",vote:0,name:"famille",src:"http://thumbs.dreamstime.com/z/famille-heureux-775568.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Inscrustation",vote:0,name:"mariage",src:"http://img.over-blog.com/600x397/4/35/94/82/Carole/Mariage-Carole-Yann---Famille---JDerache--98-.JPG"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Mixing photos",vote:0,name:"portrait",src:"https://www.pascalboegli.com/wp-content/uploads/malawi/49-portrait-enfant-malawi.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Removing character",vote:0,name:"chat",src:"http://www.enfant.com/pictures/articles/0000/310.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Adding character",vote:0,name:"skate",src:"http://www.college-romainrolland-pontivy.fr/IMG/jpg/skate.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Removing character",vote:0,name:"chien",src:"http://www.chien-guide.com/IMG/jpg/chien_4_.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Inscrustation",vote:0,name:"dessin",src:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS6qvJm1JynPFbBKlwwGOZ5RO_pIinFXLXPEF6oQCd7C7GawAvwHg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Removing character",vote:0,name:"famille",src:"https://www.higheroneaccount.com/images/student/outpages/photo-free.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Improvements",vote:0,name:"shoot",src:"http://static.independent.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2015/07/28/15/Happy-birthday.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Improvements",vote:0,name:"pont",src:"http://www.freefoto.com/images/9912/01/9912_01_1095---Tyne-Bridge--Sunset_web.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Inscrustation",vote:0,name:"mariage",src:"http://mattetsuzie.com/wp-content/uploads/2014/08/Photo-Mariage-Auberge-St-Antoine_Vieux-Quebec_15.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Improvements",vote:0,name:"golf",src:"http://visit-hungary.com/site/upload/2009/12/Golf.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Mixing photos",vote:0,name:"me",src:"https://avatars3.githubusercontent.com/u/4218815?v=3&s=460"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Mixing photos",vote:0,name:"oldpeople",src:"http://www.gettyimages.com/gi-resources/images/RoyaltyFree/109720793.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Mixing photos",vote:0,name:"fun",src:"http://media-cdn.tripadvisor.com/media/photo-s/01/1c/7a/d7/cancun.jpg"},
+    //                     {owner:"John",artist:"Protoshop",modtype:"Adding character",vote:0,name:"unicorn",src:"http://wallbasehq.net/images/medium/a-rainbow_unicorn-1526281.jpg"}
+    // ];
     $scope.homeVisible=true;
     $scope.catVisible=false;
+    $scope.mozaic=true;
+    $scope.dashboard=false;
+    $scope.resetHome = function() {
+      $scope.homeVisible=true;
+      $scope.catVisible=false;
+      $scope.mozaic=true;
+      $scope.dashboard=false;
+      $http.get('/api/pictures/last').success(function(pictures) {
+        $scope.Pics=pictures
+      });
+    }    
     $scope.askFor = function() {
+      $scope.mozaic=false;
+      $scope.dashboard=true;
     }
     $scope.explore = function() {
       $timeout(function(){$scope.catVisible=true; }, 750);
+      $scope.mozaic=true;
              
     }
     $scope.mostVotedPics = function() {
-      $scope.showLP=false;
-      $scope.showMVP=true;
-      $scope.showMCP=false;  
+      $http.get('/api/pictures/most').success(function(pictures) {
+        $scope.Pics=pictures
+      });
     }
     $scope.lastPics = function() {
-      $scope.showLP=true;
-      $scope.showMVP=false;
-      $scope.showMCP=false;  
+      $http.get('/api/pictures/last').success(function(pictures) {
+        $scope.Pics=pictures
+      }); 
     }
-    $scope.monthChallengePics = function() {
-      $scope.showLP=false;
-      $scope.showMVP=false;
-      $scope.showMCP=true;  
+    $scope.hallOfFame = function() {
+
     }
     $scope.modalPic = function(item) {
       console.log(item.src)
@@ -106,21 +78,26 @@ angular.module('modiPicsApp')
         resolve: {
           item: function () {
             return item;
+          },
+          vote: function () {
+          return $scope.vote;
           }
         }
       });
       modalInstance.result.then(function (selectedItem) {
         $scope.selected = selectedItem;
       }, function () {
-        $log.info('Modal dismissed at: ' + new Date());
       });
     };
     $scope.toggleAnimation = function () {
       $scope.animationsEnabled = !$scope.animationsEnabled;
     };
 
-    $scope.vote=function(id){
-      console.log(id)
+    $scope.vote=function(pic){
+      console.log(pic)
+      pic.vote+=1;
+      console.log(pic)
+      $http.put('/api/pictures/'+pic._id, pic);
     }
     //$scope.awesomeThings = [];
 
@@ -129,13 +106,12 @@ angular.module('modiPicsApp')
     //   socket.syncUpdates('thing', $scope.awesomeThings);
     // });
     $http.get('/api/pictures/last').success(function(pictures) {
-      console.log(pictures)
-      $scope.LPics=pictures
+      $scope.Pics=pictures
     });
     // $http.get('/api/pictures').success(function(pictures) {
     //   console.log(pictures)
     // });
-    // angular.forEach($scope.LPics, function(pic, key){
+    // angular.forEach($scope.Pics, function(pic, key){
     //   pic.date=new Date().toUTCString();
     //   $http.post('/api/pictures', pic);
     // });
@@ -156,7 +132,7 @@ angular.module('modiPicsApp')
       socket.unsyncUpdates('thing');
     });*/
   })
-.controller('ModalInstanceCtrl', function ($scope, $modalInstance, item) {
+.controller('ModalInstanceCtrl', function ($scope, $modalInstance, item, vote) {
 
   $scope.modalItem=item;
   $scope.ok = function () {
@@ -167,5 +143,8 @@ angular.module('modiPicsApp')
   };
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
+  };
+  $scope.addVote = function () {
+    $scope.result = vote(item);
   };
 });
